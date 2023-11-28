71b1ae41e38a553a8bb406d24bee169b00758845
coke <june40619@gmail.com>
2023-11-28 21:32:30 +0800

    js_簽名按鈕

---
+++ assets/js/js_dom.js
@@ -0,0 +1,12 @@
+let uid = document.querySelector('#uid');
+let students = document.querySelectorAll('.student')
+
+console.log(uid);
+console.log(students);
+
+let fullname = document.querySelector("#full-name");
+
+console.log(fullname);
+
+let btn = document.querySelector('#btn');
+
