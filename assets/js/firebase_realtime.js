3470b688d7ec4eeecbab85e2f169e1722c33648f
coke <june40619@gmail.com>
2023-12-19 21:31:26 +0800

    監聽

--- assets/js/firebase_realtime.js
+++ assets/js/firebase_realtime.js
@@ -5,4 +5,13 @@ import { Database } from './Firebase/Database.js'
 const app = await App.init()
 const database = new Database(app)
 
-database.write('coke', { name: 'COKE', Age: 18, items: [1, 2, 3, 4,] })
+// database.write('coke', { name: 'COKE', Age: 18, items: [1, 2, 3, 4,] })
+
+// let response = await database.read('david');
+// console.log(response);
+
+const onChange = (data) => {
+    console.log(data);
+}
+
+database.listen('david', onChange);
